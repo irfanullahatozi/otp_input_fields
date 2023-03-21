06d80873c8a0ed3f4a411ce28f3d8a562cd8b6e6
irfanullahtroon <irfan@troontechnologies.com>
2023-03-21 13:00:50 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [fieldIndex,setFieldIndex] = useState(0);
+  const handleKeyUp=(event)=>
+  {
+    const input = event.target;
+    let value = input.value;
+    let isValidInput = value.match(/[0-9]/gi);
+    input.value = "";
+    input.value = isValidInput ? value[0] : "";
+    if(input.nextElementSibling!==null && isValidInput){
+      input.nextElementSibling.focus();
+      setFieldIndex(fieldIndex+1)
+    }
+    if(event.key === "Backspace" && input.previousElementSibling !==null){
+      input.previousElementSibling.focus();
+      setFieldIndex(fieldIndex-1)
+    } 
+    if(fieldIndex === 5 && isValidInput){
+      submit()
+    }
+  }
+
+  const handlePaste = (event) =>{
+    const data = event.clipboardData.getData("text");
+    const value = data.split("");
+    let input = event.target
+    while(input.previousElementSibling !==null) 
+    {
+      input=input.previousElementSibling
+    }
+
+    if (value.length === 6) {
+      let count = 0;
+      while(input !== null){
+        input.value= value[count]
+        input = input.nextElementSibling;
+        count++;
+      }
+      submit()
+    }
+  }
+
+  const submit = ()=>{
+    console.log("Form is submitted!")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Enter OTP</h1>
+      <div className="otp-field">
+        <input type="text" maxLength="1" onKeyUp={handleKeyUp} onPaste={handlePaste}/>
+        <input type="text" maxLength="1" onKeyUp={handleKeyUp} onPaste={handlePaste} />
+        <input className="space" type="text" maxLength="1" onKeyUp={handleKeyUp} onPaste={handlePaste} />
+        <input type="text" maxLength="1" onKeyUp={handleKeyUp} onPaste={handlePaste} />
+        <input type="text" maxLength="1" onKeyUp={handleKeyUp} onPaste={handlePaste} />
+        <input type="text" maxLength="1" onKeyUp={handleKeyUp} onPaste={handlePaste} />
+      </div>
     </div>
   );
 }
